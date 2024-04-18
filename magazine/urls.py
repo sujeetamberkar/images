25c7fa3573d682d1b6270175cab97dd8d7b992e6
sujeetamberkar <sujeet.amberkar2002@outlook.com>
2024-04-18 21:47:59 +0530

    first commit

--- magazine/urls.py
+++ magazine/urls.py
@@ -1,7 +1,11 @@
 from os import name
 from django.urls import include, path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     # Uncomment the next line to enable the admin:
     path('',views.index, name="index"),
 ]
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
